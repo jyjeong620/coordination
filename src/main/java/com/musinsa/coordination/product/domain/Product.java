b91a3dcc01fb0b5cec9ba8a0fccf22fe0ee411ed
정준영 <jeongjy@mrblue.com>
2024-10-04 10:47:47 +0900

    feat: 상품 기본 구조 설정 및 초기데이터 삽입

---
+++ src/main/java/com/musinsa/coordination/product/domain/Product.java
@@ -0,0 +1,22 @@
+package com.musinsa.coordination.product.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long categoryId;
+
+    private Long brandId;
+
+    private BigDecimal price;
+}
---
+++ src/main/java/com/musinsa/coordination/product/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.musinsa.coordination.product.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+}
