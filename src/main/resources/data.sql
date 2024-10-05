7d8dfdcec0b4d8e390193b886dbe25115a3de44c
정준영 <jeongjy@mrblue.com>
2024-10-05 21:59:41 +0900

    fix: category 저장 시 id값 제거

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,12 @@
-INSERT INTO category (id, name)
-VALUES (1, '상의'),
-       (2, '아우터'),
-       (3, '바지'),
-       (4, '스니커즈'),
-       (5, '가방'),
-       (6, '모자'),
-       (7, '양말'),
-       (8, '액세서리');
+INSERT INTO category (name)
+VALUES ('상의'),
+       ('아우터'),
+       ('바지'),
+       ('스니커즈'),
+       ('가방'),
+       ('모자'),
+       ('양말'),
+       ('액세서리');
 
 INSERT INTO brand (name, is_enable)
 VALUES ('A', true),
