0a9b5ea461fdf178768e05334960f7cb2a14a1e8
Jeong JunYoung <70180381+jyjeong620@users.noreply.github.com>
2024-11-10 20:01:59 +0900

    Initial commit

---
+++ src/test/java/com/musinsa/coordination/CoordinationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musinsa.coordination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoordinationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
