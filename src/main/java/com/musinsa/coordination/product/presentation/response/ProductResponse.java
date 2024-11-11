69b6af76798fba6b7e48474c618b37d500f8fe1d
Jeong JunYoung <jeongjy620@gmail.com>
2024-11-11 16:03:44 +0900

    refactor: 상품 조회 API 응답결과에 id값 추가

--- src/main/java/com/musinsa/coordination/product/presentation/response/ProductResponse.java
+++ src/main/java/com/musinsa/coordination/product/presentation/response/ProductResponse.java
@@ -1,11 +1,12 @@
 package com.musinsa.coordination.product.presentation.response;
 
 import com.musinsa.coordination.product.domain.Product;
+
 import java.math.BigDecimal;
 
-public record ProductResponse(String category, String brand, BigDecimal price) {
+public record ProductResponse(Long id, String category, String brand, BigDecimal price) {
 
     public static ProductResponse from(Product product) {
-        return new ProductResponse(product.getCategoryName(), product.getBrandName(), product.getPrice());
+        return new ProductResponse(product.getId(), product.getCategoryName(), product.getBrandName(), product.getPrice());
     }
 }
